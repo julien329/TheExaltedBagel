d7aec7c15c7a04edc3da42c974e9927d9efbf6e8
Francis <francis.dalpe@polymtl.ca>
2018-11-09 19:26:01 -0500

    FD: Ice level almost done

---
+++ TheExaltedBagel/Assets/BackAndForth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour {
+
+    [SerializeField] private float movement;
+    [SerializeField] private float displacement;
+    private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+        this.startPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(this.transform.position.x + this.movement, this.transform.position.y, this.transform.position.z);
+
+        if (Mathf.Abs(this.startPosition.x - this.transform.position.x) >= displacement)
+        {
+            this.startPosition = this.transform.position;
+            movement *= -1;
+        }
+	}
+}
