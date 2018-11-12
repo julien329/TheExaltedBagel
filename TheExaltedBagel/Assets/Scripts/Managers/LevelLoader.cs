31575e624b9fcfc269ef9a44479e951eb373ebf4
Julien Charbonneau <julien_charbonneau@live.ca>
2018-11-12 12:30:09 -0500

    JC: First pass on level loading

---
+++ TheExaltedBagel/Assets/Scripts/Managers/LevelLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    static public LevelLoader instance;
+
+    private Canvas canvas;
+    private int levelIndex = 0;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void Awake()
+    {
+        LevelLoader.instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        this.canvas = GetComponent<Canvas>();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevelASync());
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    IEnumerator LoadLevelASync()
+    {
+        this.canvas.enabled = true;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(++this.levelIndex);
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+
+        this.canvas.enabled = false;
+    }
+}
--- TheExaltedBagel/Assets/Scripts/Menu/MainMenu.cs
+++ TheExaltedBagel/Assets/Scripts/Menu/MainMenu.cs
@@ -8,11 +8,22 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject lobbyScreen;
     [SerializeField] private GameObject controlsScreen;
     [SerializeField] private GameObject creditsScreen;
+    [SerializeField] private float continueTime = 0.5f;
+
+    private Animator animator;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void Awake()
+    {
+        this.animator = GetComponent<Animator>();
+        this.lobbyScreen.SetActive(true);
+    }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     public void OnClickPlay()
     {
-        print("Play");
+        this.lobbyScreen.SetActive(false);
+        this.animator.SetTrigger("ShowScroll");
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -42,4 +53,31 @@ public class MainMenu : MonoBehaviour
         this.controlsScreen.SetActive(false);
         this.creditsScreen.SetActive(false);
     }
-}
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public void OnScrollVisible()
+    {
+        StartCoroutine(DismissScroll());
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public void LoadLevel()
+    {
+        LevelLoader.instance.LoadNextLevel();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    IEnumerator DismissScroll()
+    {
+        yield return new WaitForSeconds(this.continueTime);
+
+        while (true)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                this.animator.SetTrigger("ShowBagel");
+            }
+            yield return null;
+        }
+    }
+} 
