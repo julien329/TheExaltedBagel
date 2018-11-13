8e3686bc1e9df7ce9170319c208b1503806afb78
OLI-PC\Oli <olivier.filippelli@hotmail.com>
2018-11-12 22:39:05 -0500

    Update ForestLevel

---
+++ TheExaltedBagel/Assets/Scripts/Level/ForestLevel/Castle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castle : MonoBehaviour {
+
+    [SerializeField] private GameObject door;
+    [SerializeField] private GameObject directionalLight;
+    [SerializeField] private GameObject pointLightPlayer;
+    [SerializeField] private AudioClip  audioDoorClose;
+
+    private bool isEnterCastle = true;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void Start()
+    {
+        // Enter in the castle , the close door is not suppose to be there
+        this.door.SetActive(false);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Enter Castle
+            if(isEnterCastle)
+            {
+                // Close the door
+                this.door.SetActive(true);
+                isEnterCastle = false;
+
+                //Illumination
+                directionalLight.SetActive(false);
+                pointLightPlayer.SetActive(true);
+
+                // Sound
+                SoundManager.instance.PlaySound(audioDoorClose);
+            }
+        }
+    }
+}
