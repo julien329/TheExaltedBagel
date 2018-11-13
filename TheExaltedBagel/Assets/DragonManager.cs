4a98d134426143ae0a9712404ba5c416884dba18
Francis <francis.dalpe@polymtl.ca>
2018-11-13 08:52:40 -0500

    FD: Dragon manager

---
+++ TheExaltedBagel/Assets/DragonManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonManager : MonoBehaviour {
+    
+    private Animator animator;
+
+    private void Awake()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            this.animator.SetTrigger("Bite Attack");
+            this.animator.SetTrigger("Fly Bite Attack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            this.animator.SetTrigger("Fly Idle");
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            this.animator.SetTrigger("Fly Take Damage");
+
+        }
+    }
+}
